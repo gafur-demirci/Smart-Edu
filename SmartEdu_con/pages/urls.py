2d5e972dd34f354db2a4f9549de4d98fb8b0aae0
Gafur <gafur.gs.68@gmail.com>
2021-11-17 21:12:41 +0300

    About pages url modified

--- SmartEdu_con/pages/urls.py
+++ SmartEdu_con/pages/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('about/', views.about, name="about"),
     #path(route, view, opt olarak da name verilir)
 ]
