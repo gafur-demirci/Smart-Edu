5450f20588eb6e10fdb9551c92f548d65da7e350
Gafur <gafur.gs.68@gmail.com>
2021-11-19 00:16:31 +0300

    Added MEDIA FILES

--- SmartEdu_con/SmartEdu/settings.py
+++ SmartEdu_con/SmartEdu/settings.py
@@ -125,4 +125,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MEDIA FILES
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
