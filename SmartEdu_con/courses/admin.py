82bafa7a916a01684189f1d0179a225a3f24a2aa
Gafur <gafur.gs.68@gmail.com>
2021-11-19 23:39:20 +0300

    Category table saved to admin panel

--- SmartEdu_con/courses/admin.py
+++ SmartEdu_con/courses/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from . models import Course
+from . models import Course, Category
 
 # Register your models here.
 # Course modelini admin alanında görmek için kayıt işlemi
 # admin.site.register(Course)
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'available')
+    list_display = ('name', 'category', 'available')
     list_filter = ('available',)
     search_fields = ('name', 'description')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name',)}
