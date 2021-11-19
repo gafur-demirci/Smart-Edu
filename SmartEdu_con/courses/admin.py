2edbfc518667863576c5d3323b8ec0bc52648cd6
Gafur <gafur.gs.68@gmail.com>
2021-11-19 18:03:18 +0300

    Admin panel modified

--- SmartEdu_con/courses/admin.py
+++ SmartEdu_con/courses/admin.py
@@ -3,4 +3,9 @@ from . models import Course
 
 # Register your models here.
 # Course modelini admin alanında görmek için kayıt işlemi
-admin.site.register(Course)
+# admin.site.register(Course)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'available')
+    list_filter = ('available',)
+    search_fields = ('name', 'description')
