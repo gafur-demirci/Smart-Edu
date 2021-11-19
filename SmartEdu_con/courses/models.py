137270938305d007d1230a8369fba7eff323f03c
Gafur <gafur.gs.68@gmail.com>
2021-11-19 23:30:06 +0300

    Model updated

--- SmartEdu_con/courses/models.py
+++ SmartEdu_con/courses/models.py
@@ -4,6 +4,9 @@ class Category(models.Model):
     name = models.CharField(max_length=50, null=True)
     slug = models.SlugField(max_length=50, unique= True, null=True)
 
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class Course(models.Model):
     name = models.CharField(max_length=200, unique=True, blank=True)
