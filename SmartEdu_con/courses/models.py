be1ac627336453696863894efda4599c7c5b4db8
Gafur <gafur.gs.68@gmail.com>
2021-11-20 14:57:40 +0300

    Tag table created

--- SmartEdu_con/courses/models.py
+++ SmartEdu_con/courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=50, null=True)
     slug = models.SlugField(max_length=50, unique= True, null=True)
@@ -7,7 +9,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique= True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Course(models.Model):
     name = models.CharField(max_length=200, unique=True, blank=True)
     category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
