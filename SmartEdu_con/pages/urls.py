bd9c72758192171993acbf9905d067dc35d615b0
Gafur <gafur.gs.68@gmail.com>
2021-11-21 18:38:37 +0300

    updated

--- SmartEdu_con/pages/urls.py
+++ SmartEdu_con/pages/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from . import views
+from pages.views import IndexView, AboutView, ContactView
+# from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('about/', views.about, name="about"),
-    path('contact/', views.contact, name="contact"),
+    # path('', views.index, name="index"),
+    # path('about/', views.about, name="about"),
+    # path('contact/', views.contact, name="contact"),
+    path('', IndexView.as_view(), name="index"),
+    path('about/', AboutView.as_view(), name="about"),
+    path('contact/', ContactView.as_view(), name="contact"),
     #path(route, view, opt olarak da name verilir)
 ]
--- SmartEdu_con/pages/views.py
+++ SmartEdu_con/pages/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from courses.models import Course
 # from django.http import HttpResponse
 
+# Function Based Views
 # Index View
-def index(request):
-    return render(request, 'index.html')
-    # return HttpResponse("<h1>Index Sayfasi</h1>")
+# def index(request):
+#     return render(request, 'index.html')
+#     # return HttpResponse("<h1>Index Sayfasi</h1>")
 
-# About View
-def about(request):
-    return render(request, 'about.html')
+# # About View
+# def about(request):
+#     return render(request, 'about.html')
 
-# Contact View
-def contact(request):
-    return render(request, 'contact.html')
+# # Contact View
+# def contact(request):
+#     return render(request, 'contact.html')
+
+# Class Based View (TemplateView)
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['courses'] = Course.objects.filter(available = True).order_by('-date')[:3]
+        context['total_course'] = Course.objects.filter(available = True).count()
+        return context
+    
+class AboutView(TemplateView):
+    template_name = 'about.html'
+class ContactView(TemplateView):
+    template_name = 'contact.html'
