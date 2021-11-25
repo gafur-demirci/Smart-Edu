c59fc63b7bfc6ae0de0ec2034154937afa9f6710
Gafur <gafur.gs.68@gmail.com>
2021-11-25 16:20:29 +0300

    modified by accounts app

--- SmartEdu_con/accounts/views.py
+++ SmartEdu_con/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def user_login(request):
+    return render(request, 'login.html')
+
+def user_register(request):
+    return render(request, 'register.html')
+
+def user_dashboard(request):
+    pass
+
+def user_logout(request):
+    pass
