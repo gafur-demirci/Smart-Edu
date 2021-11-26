b1eec38034eee1cc81b69b8b3c8ded6d5983f99e
Gafur <gafur.gs.68@gmail.com>
2021-11-26 20:41:20 +0300

    views updated by enroll-release

--- SmartEdu_con/courses/views.py
+++ SmartEdu_con/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from . models import Course, Category, Tag
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -9,6 +10,7 @@ def course_list(request, category_slug = None, tag_slug = None):
     tag_page = None
     categories = Category.objects.all()
     tags = Tag.objects.all()
+    current_user = request.user
 
     if category_slug != None:
         category_page = get_object_or_404(Category, slug = category_slug)
@@ -17,7 +19,15 @@ def course_list(request, category_slug = None, tag_slug = None):
         tag_page = get_object_or_404(Tag, slug=tag_slug)
         courses = Course.objects.filter(available = True, tags = tag_page)
     else:
-        courses = Course.objects.all().order_by('-date')
+        # courses = Course.objects.all().order_by('-date')
+        if current_user.is_authenticated:
+            enrolled_courses = current_user.courses_joined.all()
+            courses = Course.objects.all().order_by('-date')
+            for course in enrolled_courses:
+                courses = courses.exclude(id = course.id)
+        else:
+            courses = Course.objects.all().order_by('-date')
+
     context = {
         'courses' : courses,
         'categories' : categories,
@@ -27,11 +37,23 @@ def course_list(request, category_slug = None, tag_slug = None):
     return render(request, 'courses.html', context)
 
 def course_detail(request, category_slug, course_id):
-    course = Course.objects.get(category__slug = category_slug, id = course_id)
     categories = Category.objects.all()
     tags = Tag.objects.all()
+
+    current_user = request.user
+
+    course = Course.objects.get(category__slug = category_slug, id = course_id)
+    courses = Course.objects.all().order_by('-date')
+
+    if current_user.is_authenticated:
+        enrolled_courses = current_user.courses_joined.all()
+
+    else:
+        enrolled_courses = courses
+
     context = {
         'course' : course,
+        'enrolled_courses' : enrolled_courses,
         'categories' : categories,
         'tags' : tags
     }
