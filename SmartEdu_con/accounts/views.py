2788824b971bc0ec3e4822da57964c761cbe5126
Gafur <gafur.gs.68@gmail.com>
2021-11-26 15:20:05 +0300

    dashboard added views and course model

--- SmartEdu_con/accounts/views.py
+++ SmartEdu_con/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from . forms import LoginForm, RegisterForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def user_login(request):
@@ -54,5 +55,7 @@ def user_logout(request):
     logout(request)
     return redirect('index')
 
+@login_required(login_url='login')
 def user_dashboard(request):
-    pass
+    current_user = request.user
+    return render(request, 'dashboard.html')
