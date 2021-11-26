c226be372fe339ff7e17daa3c1249e26215caa0e
Gafur <gafur.gs.68@gmail.com>
2021-11-26 20:43:21 +0300

    enroll-release views added

--- SmartEdu_con/accounts/views.py
+++ SmartEdu_con/accounts/views.py
@@ -3,6 +3,8 @@ from . forms import LoginForm, RegisterForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from courses.models import Course
 
 # Create your views here.
 def user_login(request):
@@ -58,4 +60,23 @@ def user_logout(request):
 @login_required(login_url='login')
 def user_dashboard(request):
     current_user = request.user
-    return render(request, 'dashboard.html')
+    courses = current_user.courses_joined.all().order_by('-date')
+    context = {
+        'courses' : courses
+    }
+
+    return render(request, 'dashboard.html', context)
+
+def enroll_the_course(request):
+    course_id = request.POST['course_id']
+    user_id = request.POST['user_id']
+    course = Course.objects.get(id=course_id)
+    user = User.objects.get(id = user_id)
+    course.students.add(user)
+    return redirect('dashboard')
+
+def release_the_course(request):
+    course = Course.objects.get(id = request.POST['course_id'])
+    user = User.objects.get(id = request.POST['user_id'])
+    course.students.remove(user)
+    return redirect('dashboard')
